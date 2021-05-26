ed8177cffccb318d45625135a276c9a4ff956895
ajk787 <84345134+ajk787@users.noreply.github.com>
2021-05-25 20:43:18 -0400

    updated to validate choice

--- game.py
+++ game.py
@@ -7,3 +7,19 @@ user_choice = input("Please choose one of 'rock', 'paper', 'scissors':")
 
 #print(user_choice)
 print("USER CHOICE: ", user_choice)
+
+
+# validate input so that it is only one of the expected values
+# will stop program before it tries to do anything else
+# will have it make user run it again
+
+
+
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID, KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+exit()
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
