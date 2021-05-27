526a80ba6f3a6d811a3805baf0744ce216b1bbd6
ajk787 <84345134+ajk787@users.noreply.github.com>
2021-05-27 18:21:43 -0400

    added conditions for winner

--- game.py
+++ game.py
@@ -2,9 +2,10 @@
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!")
 
-user_choice = input("Please choose one of 'rock', 'paper', 'scissors':")
+print("Welcome USER_NAME to Rock, Paper, Scissors, Shoot!")
+
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
 
 #print(user_choice)
 print("USER CHOICE: ", user_choice)
@@ -15,17 +16,38 @@ print("USER CHOICE: ", user_choice)
 # will have it make user run it again
 
 
-
-
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
     print("VALID, KEEP GOING")
 else:
     print("OOPS, invalid input. Please try again.")
-
+    exit () 
 
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
+
+#condition checks for winner
+if (user_choice == computer_choice):
+    print ("You tied this round")
+
+elif ((user_choice == "rock") and computer_choice == "paper"):
+   print ("Paper beats rock, sorry you lose!")
+
+elif ((user_choice == "rock") and computer_choice == "scissors"):
+   print ("Rock beats scissors, great you win!")
+
+elif ((user_choice == "paper") and computer_choice == "rock"):
+  print ("Paper beats rock, great you win!")
+
+elif ((user_choice == "paper") and computer_choice == "scissors"):
+   print ("Scissors beats paper, sorry you lose!")   
+
+elif ((user_choice == "scissors") and computer_choice == "rock"):
+   print ("Rock beats scissors, sorry you lose!")
+
+elif ((user_choice == "scissors") and computer_choice == "paper"):
+   print ("Scissors beats paper, great you win!")     
+    
 
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
 
